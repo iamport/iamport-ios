18bdcc56520b7aabfdd11e0b22062e3e99c81849
bingbong <akrasias2@naver.com>
2021-02-16 16:22:54 +0900

    feat : 샘플앱 결과 화면 UI 약간 수정

--- Example/iamport-ios/PaymentResultViewController.swift
+++ Example/iamport-ios/PaymentResultViewController.swift
@@ -24,27 +24,35 @@ class PaymentResultViewController: UIViewController, UIGestureRecognizerDelegate
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.interactivePopGestureRecognizer?.delegate = self
-
         print("종료 됐어요")
 
         let successColor = UIColor.green
         let failColor = UIColor.orange
+        let justCloseColor = UIColor.darkGray
         impResponseRelay.asObservable().subscribe { iamportResponseEvent in
             var color: UIColor = failColor
+            var desc = "Just Close"
+
             // 성공 케이스
             if let iamportResponse = iamportResponseEvent.element, let response = iamportResponse {
-                dump(iamportResponse)
+//                dump(iamportResponse)
                 if (self.isSuccess(response)) {
                     color = successColor
                 }
+
+                desc = iamportResponse?.description ?? ""
+            } else {
+                color = justCloseColor
             }
+
+            print(desc)
             self.view.backgroundColor = color
         }.disposed(by: disposeBag)
     }
 
     // imp_success, success 해당 값을 맹신할 수 없습니다.
-    // 뱅크페이 실시간 계좌이체의 경우 해당 값이 전달되지 않는 케이스가 있습니다.
-    // 결과 콜백을 받으면 Iamport REST API 등을 통해 실제 결제 여부를 체크하셔야 합니다.
+    // 뱅크페이 실시간 계좌이체는 해당 값이 전달되지 않는 케이스가 있습니다.
+    // 결과 콜백을 받으면, Iamport REST API 등을 통해 "실제 결제 여부" 를 체크하셔야 합니다.
     private func isSuccess(_ iamportResponse: IamPortResponse) -> Bool {
         iamportResponse.imp_success ?? false || iamportResponse.success ?? false
     }
