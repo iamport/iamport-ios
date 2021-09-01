bedc959b1e7a06c0c8ff47396be299260dff363f
bingbong <akrasias2@naver.com>
2021-09-01 10:23:34 +0900

    init spm

---
+++ Tests/iamport-iosTests/iamport_iosTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import iamport_ios
+
+    final class iamport_iosTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(iamport_ios().text, "Hello, World!")
+        }
+    }
